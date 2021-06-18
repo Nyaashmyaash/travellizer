7a250024f62de2fe0215943453475c9d4fb20388
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-18 08:41:36 +0700

    add changes

--- src/main/java/com/nyash/travellizer/model/entity/ticket/Ticket.java
+++ src/main/java/com/nyash/travellizer/model/entity/ticket/Ticket.java
@@ -1,10 +1,56 @@
 package com.nyash.travellizer.model.entity.ticket;
 
+import com.nyash.travellizer.common.generator.text.StringGenerator;
+import com.nyash.travellizer.common.infra.util.Checks;
+import lombok.Setter;
+
+/**
+ * Trip ticket
+ *
+ * @author Nyash
+ */
+@Setter
 public class Ticket {
 
+    /**
+     * Size of the generated ticket number
+     */
+    public static final int TICKET_NUMBER_SIZE = 32;
+
+    /**
+     * Link to the underlying trip
+     */
     private String tripId;
 
+    /**
+     * Client name
+     */
     private String name;
 
+    /**
+     * Auto-generated ticket identifier(usually random)
+     */
     private String uid;
+
+    public String getTripId() {
+        return tripId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    /**
+     * Generates system-unique ticket number
+     *
+     * @param generator
+     */
+    public void generateUid(final StringGenerator generator) {
+        Checks.checkParameter(generator != null, "StringGenerator should be initialized");
+        uid = generator.generate();
+    }
 }
