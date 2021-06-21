95b167971b84328d3dbd3915aa027a89fc2999d8
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-22 04:40:04 +0700

    add changes

---
+++ src/main/java/com/nyash/travellizer/model/entity/user/Role.java
@@ -0,0 +1,12 @@
+package com.nyash.travellizer.model.entity.user;
+
+/**
+ * User role that affects permissions of the current user
+ * @author Nyash
+ *
+ */
+public enum Role {
+    USER,
+    MANAGER,
+    ADMIN
+}
