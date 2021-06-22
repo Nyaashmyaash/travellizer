71fe87453ef5e7d3d97024f548826cd307e23278
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-22 09:28:15 +0700

    add changes

--- src/main/java/com/nyash/travellizer/service/user/UserService.java
+++ src/main/java/com/nyash/travellizer/service/user/UserService.java
@@ -1,4 +1,46 @@
 package com.nyash.travellizer.service.user;
 
+import com.nyash.travellizer.model.entity.user.User;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Provides API for the user management
+ * @author Nyash
+ *
+ */
 public interface UserService {
+
+    /**
+     * Saves(creates or modifies) specified user instance
+     * @param user
+     */
+    void save(User user);
+
+    /**
+     * Returns user with specified identifier boxed into Optional
+     * @param userId
+     * @return
+     */
+    Optional<User> findById(int userId);
+
+    /**
+     * Returns user with specified username
+     * @param userName
+     * @return
+     */
+    Optional<User> findByUserName (String userName);
+
+    /**
+     * Delete user with specified identifier
+     * @param userId
+     */
+    void delete(int userId);
+
+    /**
+     * Returns all users
+     * @return
+     */
+    List<User> findAll();
 }
