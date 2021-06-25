c85effe235a6627e9a04e105a4e74213ce652159
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-25 22:18:00 +0700

    add changes

--- src/main/java/com/nyash/travellizer/common/infra/exception/base/AppException.java
+++ src/main/java/com/nyash/travellizer/common/infra/exception/base/AppException.java
@@ -1,6 +1,11 @@
 package com.nyash.travellizer.common.infra.exception.base;
 
-public class AppException extends RuntimeException{
+/**
+ * Base class for all application-specific exceptions
+ * @author Nyash
+ *
+ */
+public abstract class AppException extends RuntimeException{
 
     private static final long serialVersionUID = 7837501112802868980L;
 
---
+++ src/main/java/com/nyash/travellizer/common/infra/exception/flow/HttpRestException.java
@@ -0,0 +1,24 @@
+package com.nyash.travellizer.common.infra.exception.flow;
+
+import com.nyash.travellizer.common.infra.exception.FlowException;
+
+/**
+ * Invoked when web server returns error status code (4xx-5xx)
+ *
+ * @author Nyash
+ *
+ */
+public class HttpRestException extends FlowException {
+
+    private static final long serialVersionUID = -6408338683345172869L;
+
+    /**
+     * True if it's client error(4xx) or server error (5xx)
+     */
+    private final boolean client;
+
+    public HttpRestException(String message, boolean client) {
+        super(message);
+        this.client = client;
+    }
+}
