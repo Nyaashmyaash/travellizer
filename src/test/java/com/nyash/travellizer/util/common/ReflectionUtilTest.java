2a951387cc1e18caec6ea299ce5a0ddee227a4c3
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-01 07:08:27 +0700

    add changes

--- src/test/java/com/nyash/travellizer/util/common/ReflectionUtilTest.java
+++ src/test/java/com/nyash/travellizer/util/common/ReflectionUtilTest.java
@@ -1,5 +1,7 @@
 package com.nyash.travellizer.util.common;
 
+import com.nyash.travellizer.common.infra.util.annotations.Ignore;
+
 /**
  * Verifies functionality of the {@link ReflectionUtil} unit
  *
@@ -9,4 +11,48 @@ package com.nyash.travellizer.util.common;
 public class ReflectionUtilTest {
 
 
+}
+
+class BaseSource {
+    private int BaseField;
+}
+
+class BaseDestination {
+    private int BaseField;
+}
+
+class Source extends BaseSource {
+    private int value;
+
+    private String text;
+
+    @Ignore
+    private int ignored = 2;
+
+    private static int staticField;
+
+    private final int finalfield = 0;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
+
+class Destination extends BaseDestination {
+    private int value;
+
+    private int ignored;
+
+    private int staticField;
+
+    private int finalField = 0;
+
+    public int getValue() {
+        return value;
+    }
+}
+
+class Restricted {
+    public Restricted(int value) {
+    }
 }
