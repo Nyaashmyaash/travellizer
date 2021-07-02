b4c1a4474612065cb52bbcf975051b6f717ed5ce
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-03 02:25:28 +0700

    add changes

--- src/test/java/com/nyash/travellizer/util/transform/SimpleDTOTransformerTest.java
+++ src/test/java/com/nyash/travellizer/util/transform/SimpleDTOTransformerTest.java
@@ -3,7 +3,11 @@ package com.nyash.travellizer.util.transform;
 import com.nyash.travellizer.common.model.transform.Transformer;
 import com.nyash.travellizer.common.model.transform.impl.CachedFieldProvider;
 import com.nyash.travellizer.common.model.transform.impl.SimpleDTOTransformer;
+import com.nyash.travellizer.model.entity.base.AbstractEntity;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.time.DayOfWeek;
+import java.util.UUID;
 
 /**
  * Verifies functionality of the {@link SimpleDTOTransformer} unit
@@ -14,8 +18,18 @@ import org.junit.jupiter.api.BeforeEach;
 public class SimpleDTOTransformerTest {
     private Transformer transformer;
 
-//    @BeforeEach
-//    public void setup() {
-//        transformer = new SimpleDTOTransformer(new CachedFieldProvider(), new StubTransformableProvider());
-//    }
+    @BeforeEach
+    public void setup() {
+        transformer = new SimpleDTOTransformer(new CachedFieldProvider(), new StubTransformableProvider());
+    }
+
+    public static class Source extends AbstractEntity {
+        int value;
+
+        String text;
+
+        DayOfWeek day;
+
+        UUID id;
+    }
 }
