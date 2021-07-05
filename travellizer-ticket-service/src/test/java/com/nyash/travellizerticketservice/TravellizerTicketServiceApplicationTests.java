9c99969f162150d4af60e4b35939470a68df7bc0
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-07-06 02:24:18 +0700

    add changes

---
+++ travellizer-ticket-service/src/test/java/com/nyash/travellizerticketservice/TravellizerTicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyash.travellizerticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellizerTicketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
